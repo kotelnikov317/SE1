86ac2b10307e1b6fbe558ffe7d13d578822c651c
kotelnikov17 <kotelnikovgithub@mail.ru>
2024-09-05 22:21:38 +0500

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -4,4 +4,13 @@ using namespace std;
 
 int main()
 {
+    system("chcp 1251");
+    system("cls");
+    int x,y;
+    cout << "¬ведите два числа:\n";
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    return 0;
 }
